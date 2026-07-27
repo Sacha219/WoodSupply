69de804531d6db5f87da8ce3a8e8e6955ff5adf2
Sasha <saaachk.a2007@gmail.com>
2026-07-27 17:57:43 +0300

    1 хорошая версия

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ← для корневого домена (wood-supply.ru)
-  // или base: './', // если сайт в корне — тоже работает, но лучше '/'
+base: '/WoodSupply/'  // или base: './', // если сайт в корне — тоже работает, но лучше '/'
 })
